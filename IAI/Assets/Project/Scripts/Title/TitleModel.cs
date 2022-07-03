380f6acfcf24f8298c31b62304cf75f3af3c759e
iiweis <iiweis.net@gmail.com>
2022-07-04 00:20:47 +0900

    fix bug

--- IAI/Assets/Project/Scripts/Title/TitleModel.cs
+++ IAI/Assets/Project/Scripts/Title/TitleModel.cs
@@ -6,5 +6,10 @@ public class TitleModel : MonoBehaviour
     /// <summary>
     /// ゲームシーンに遷移する。
     /// </summary>
-    public void TransitionToGameScene() => SceneManager.LoadScene(SceneNames.Main);
+    public void TransitionToGameScene()
+    {
+        // 前回の記録が残っている可能性があるのでリセットしてから遷移する
+        GameManager.Instance.Reset();
+        SceneManager.LoadScene(SceneNames.Main);
+    }
 }
